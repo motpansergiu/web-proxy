9b6d18d63042e999cfcb4dc7b039940ef33fc22e
Serghei Motpan <smotpan@tacitknowledge.com>
2018-04-28 22:05:48 +0300

    Ensure the cassandra connection is closed

--- data-warehouse/src/main/java/dvl/srg/App.java
+++ data-warehouse/src/main/java/dvl/srg/App.java
@@ -23,32 +23,37 @@ public final class App {
     private static final String CASSANDRA_PROPERTIES = "cassandra.properties";
 
     public static void main(String[] args) {
-        ApplicationProperties applicationProperties = null;
-        CassandraProperties cassandraProperties = null;
+        CassandraConnectorManager connectorManager = null;
         try {
-            logger.info("Loading application properties");
-            final String path = App.class.getClassLoader().getResource("").getPath();
-            applicationProperties = PropertyFileLoader.loadApplicationProperties(path + APPLICATION_PROPERTIES);
-            cassandraProperties = PropertyFileLoader.loadCassandraProperties(path + CASSANDRA_PROPERTIES);
-        } catch (IOException e) {
-            logger.error("Cannot load application properties file!", e);
-            System.exit(-1);
+            ApplicationProperties applicationProperties = null;
+            CassandraProperties cassandraProperties = null;
+            try {
+                logger.info("Loading application properties");
+                final String path = App.class.getClassLoader().getResource("").getPath();
+                applicationProperties = PropertyFileLoader.loadApplicationProperties(path + APPLICATION_PROPERTIES);
+                cassandraProperties = PropertyFileLoader.loadCassandraProperties(path + CASSANDRA_PROPERTIES);
+            } catch (IOException e) {
+                logger.error("Cannot load application properties file!", e);
+                System.exit(-1);
+            }
+
+            connectorManager = new CassandraConnectorManager(new CassandraConnector(), cassandraProperties);
+            connectorManager.setUpCassandraKeyspace();
+
+            try {
+                final ServerSocketManager socketManager = new ServerSocketManager(applicationProperties);
+                final ReactorManager reactorManager = new ReactorManager(connectorManager.getSession(),
+                        socketManager.start(), new Reactor(new AtomicBoolean(true)));
+
+                reactorManager.run();
+            } catch (final IOException e) {
+                logger.error("Cannot start reactor!", e);
+                System.exit(-1);
+            }
+        } finally {
+            if (null != connectorManager) {
+                connectorManager.close();
+            }
         }
-
-        final CassandraConnectorManager connectorManager = new CassandraConnectorManager(new CassandraConnector(), cassandraProperties);
-        connectorManager.setUpCassandraKeyspace();
-
-        try {
-            final ServerSocketManager socketManager = new ServerSocketManager(applicationProperties);
-            final ReactorManager reactorManager = new ReactorManager(connectorManager.getSession(),
-                    socketManager.start(), new Reactor(new AtomicBoolean(true)));
-
-            reactorManager.run();
-        } catch (final IOException e) {
-            logger.error("Cannot start reactor!", e);
-            System.exit(-1);
-        }
-
-        connectorManager.close();
     }
 }
